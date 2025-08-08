cdfe1897adcacba23894e03936bb35bcb8b4d45b
ivannakm <ivadatalover@gmail.com>
2025-08-08 19:13:03 +0200

    Finish mondodb app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
 import { setupServer } from './server.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
 
-setupServer();
+const bootstrap = async () => {
+  await initMongoConnection();
+  setupServer();
+};
+
+bootstrap();
